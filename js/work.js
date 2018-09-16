17ad64efbd7c3ad9b66b5a4295f2e628a2c0456f
Okeke Mac-Anthony <ekenemacanthony@gmail.comgit config --global user.name mac-anthonygit config user.name mac_anthonygit config user.email ekenemacanthony@gmail.com>
2018-09-16 23:01:36 +0100

    Day8,i added a 2 new html and css file and also a work.js file.

---
+++ js/work.js
@@ -0,0 +1,23 @@
+
+
+function getStuff(){
+  document.querySelector('#tuna').onclick=talk; /* this is saying go into the document and get the first element with an id of tuna */
+
+  var list = document.querySelectorAll('#tuna');
+  // list[1].onclick=doSomething;
+  for(var i=0; i < list.length; i++){
+    list[1].onclick=talk;
+  }
+}
+
+
+
+function talk(){
+  alert('i am the first');
+}
+window.onload=getStuff;
+
+// function doSomething(){
+//   alert('i am the third')
+// }
+// window.onload=getStuff;
