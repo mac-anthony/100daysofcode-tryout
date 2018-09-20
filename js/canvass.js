cdb4503679be89d6572060aaf418d148fb50ec7a
Okeke Mac-Anthony <ekenemacanthony@gmail.comgit config --global user.name mac-anthonygit config user.name mac_anthonygit config user.email ekenemacanthony@gmail.com>
2018-09-20 21:46:15 +0100

    modified canvas.js file

--- js/canvass.js
+++ js/canvass.js
@@ -27,14 +27,51 @@ function executeFirst() {
     // canvas.fillStyle=g;
     // canvas.fillRect(0,0,100,100);
 
-  canvas.begingPath();
-  canvas.moveTo(50,50);
-  canvas.lineTo(70,250);
-  canvas.lineTo(300,200);
-  canvas.closePath();
-  canvas.stroke();
+  // canvas.begingPath();
+  // canvas.moveTo(50,50);
+  // canvas.lineTo(70,250);
+  // canvas.lineTo(300,200);
+  // canvas.closePath();
+  // canvas.stroke();
 
 
+  //CANVAS TRANSFORMATION:
+
+  canvas.font='bold 22px Tahoma'
+  canvas.textAlign='start';
+  canvas.fillText('start', 10,30);
+
+  canvas.translate(100,150);
+  canvas.fillText('after translate', 0, 0);
+
+  canvas.rotate(1);
+  canvas.fillText('after rotate', 0,0);
+
+  canvas.scale(1.5,4);
+  canvas.fillText('after scale', 0,20);
+
+  //WORKING WITH IMAGES ON THE CANVAS
+
+  var pic = new Image();
+  pic.src="image/unsplash5.jpeg";
+  pic.addEventListener('load', function(){
+    canvas.drawImage(pic,0,0,x.Width,x.height)
+  }, false)
+
+
+  //USING CANVAS ANIMATION FOR GAMES
+
+  window.addEventListener('mousemove', Animate, false)
 };
+
+function Animate (e) {
+  // canvas.clearRect(0,0,600,400); // what this does is it takes a shape as a parameter and it clears all the pixels in it.
+
+  var xPosition = e.clientX; //this is to get the position of x in the canvas
+
+  var yPosition = e.clientY;
+
+  canvas.fillRect(xPosition-50,yPosition-50,100,100); // this is saying make a rectangle from the x to the y position(whenever the mouse is moving around)
+}
 
 window.addEventListener('load', executeFirst, false)
